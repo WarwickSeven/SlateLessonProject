fa06350668f072c61cf970e22963184cbffd73c7
WarwickSeven <WarwickSeven@gmail.com>
2022-11-27 22:13:52 +0400

    Lets start

---
+++ Source/SlateLessonProject/SlateLessonProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SlateLessonProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SlateLessonProject, "SlateLessonProject" );
+ 
